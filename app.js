09381b8510219f62aa661225354e3e1a14be0f2b
rishipurwar1 <rishipurwar007@gmail.com>
2021-09-22 17:19:24 +0530

    add cors

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
-const connectDB = require('./config/db');
+var cors = require("cors");
+const port = process.env.PORT || 5000;
+const connectDB = require("./config/db");
 
 // routes
-const invoices = require('./routes/api/invoices');
+const invoices = require("./routes/api/invoices");
 
 // Init Middleware
 app.use(express.json({ extended: false }));
+var corsOptions = {
+  origin: "http://localhost:5000/",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', "*");
-    res.setHeader('Access-Control-Allow-Methods', "POST,GET,OPTIONS");
-    res.setHeader('Access-Control-Allow-Headers', "Content-Type, Authorization");
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
 });
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Connect Database
 connectDB();
 
-app.get('/api', (req, res) => {
-    res.render('index')
+app.get("/api", (req, res) => {
+  res.render("index");
 });
 
-app.use('/api', invoices);
+app.use("/api", invoices);
 
 app.listen(port, () => {
-    console.log('Server is started');
-})
+  console.log("Server is started");
+});
