5c5b7ad48cffea7c2f5d0c8d1c81012c7345d29c
rishipurwar1 <rishipurwar007@gmail.com>
2021-09-23 18:24:01 +0530

    modify mongo schema

--- models/Invoice.js
+++ models/Invoice.js
@@ -1,67 +1,55 @@
 // models/Book.js
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const InvoiceSchema = new mongoose.Schema({
-    "id": {
-        "type": "String"
-    },
-    "paymentDue": {
-        "type": "Date"
-    },
-    "description": {
-        "type": "String"
-    },
-    "paymentTerms": {
-        "type": "Number"
-    },
-    "clientName": {
-        "type": "String"
-    },
-    "clientEmail": {
-        "type": "String"
-    },
-    "status": {
-        "type": "String"
-    },
-    "senderAddress": {
-        "street": {
-            "type": "String"
-        },
-        "city": {
-            "type": "String"
-        },
-        "postCode": {
-            "type": "String"
-        },
-        "country": {
-            "type": "String"
-        }
-    },
-    "clientAddress": {
-        "street": {
-            "type": "String"
-        },
-        "city": {
-            "type": "String"
-        },
-        "postCode": {
-            "type": "String"
-        },
-        "country": {
-            "type": "String"
-        }
-    },
-    "items": {
-        "type": [
-            "Mixed"
-        ]
-    },
-    "total": {
-        "type": "Number"
-    }
+  paymentDue: {
+    type: "Date",
+  },
+  description: {
+    type: "String",
+  },
+  paymentTerms: {
+    type: "Number",
+  },
+  clientName: {
+    type: "String",
+  },
+  clientEmail: {
+    type: "String",
+  },
+  status: {
+    type: "String",
+  },
+  streetAddress: {
+    type: "String",
+  },
+  city: {
+    type: "String",
+  },
+  postCode: {
+    type: "String",
+  },
+  country: {
+    type: "String",
+  },
+  clientStreetAddress: {
+    type: "String",
+  },
+  clientCity: {
+    type: "String",
+  },
+  clientPostCode: {
+    type: "String",
+  },
+  clientCountry: {
+    type: "String",
+  },
+  items: {
+    type: ["Mixed"],
+  },
 });
 
-InvoiceSchema.set('timestamps', true);
+InvoiceSchema.set("timestamps", true);
 
-module.exports = Invoice = mongoose.model('invoice', InvoiceSchema);
+module.exports = Invoice = mongoose.model("invoice", InvoiceSchema);
